9cfaa5da3d191ee53d5552ee3bd189999ba07964
Pruthviraj Mhaske <pruthvirajmhaske51@gmail.com>
2022-05-09 19:24:49 +0530

    First commit; Project upload

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("", views.index, name='main'),
+    path("about", views.about, name='about'),
+    path("contact_us", views.contact_us, name='contact_us'),
+    path("services", views.services, name='services'),
+
+]
